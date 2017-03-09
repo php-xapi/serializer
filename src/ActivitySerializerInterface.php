98c8716d3a3c4306b8a072f173cb947b3b758b36
Jérôme Parmentier <jerome.parmentier@acensi.fr>
2017-03-09 11:13:26 +0100

    Add exceptions

--- src/ActivitySerializerInterface.php
+++ src/ActivitySerializerInterface.php
@@ -12,6 +12,8 @@
 namespace Xabbuh\XApi\Serializer;
 
 use Xabbuh\XApi\Model\Activity;
+use Xabbuh\XApi\Serializer\Exception\ActivityDeserializationException;
+use Xabbuh\XApi\Serializer\Exception\ActivitySerializationException;
 
 /**
  * Serialize and deserialize {@link Activity activities}.
@@ -25,6 +27,8 @@ interface ActivitySerializerInterface
      *
      * @param Activity $activity The activity to serialize
      *
+     * @throws ActivitySerializationException When the serialization fails
+     *
      * @return string The serialized activity
      */
     public function serializeActivity(Activity $activity);
@@ -33,6 +37,8 @@ interface ActivitySerializerInterface
      * Parses a serialized activity.
      *
      * @param string $data The serialized activity
+     *
+     * @throws ActivityDeserializationException When the deserialization fails
      *
      * @return Activity The parsed activity
      */
